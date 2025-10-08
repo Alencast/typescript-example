e5a41ab4e7029e90842a1e4a899d00ff1f2adf18
Robson Alencastro <90284550+Alencast@users.noreply.github.com>
2025-10-08 07:28:37 -0300

    Docs: organizacao das pastas

--- src/q6/q6.test.ts
+++ src/q6/q6.test.ts
@@ -5,13 +5,13 @@ test('testa cálculo da área de Retângulo e Círculo', () => {
   expect(ret.calcular()).toBe(50); // 5 * 10 = 50
 
   const circ = new Circulo(3);
-  expect(circ.calcular()).toBeCloseTo(28.274, 3); // π * 3² ≈ 28.274
+  expect(circ.calcular()).toBeCloseTo(28.274, 3); // π * 3² 
 
-  // testar novamente com atributos diferentes
+  // testar com atributos diferentes
   ret.largura = 7;
   ret.altura = 2;
   expect(ret.calcular()).toBe(14); // 7 * 2 = 14
 
   circ.raio = 4;
-  expect(circ.calcular()).toBeCloseTo(50.265, 3); // π * 4² ≈ 50.265
+  expect(circ.calcular()).toBeCloseTo(50.265, 3); // π * 4² 
 });
